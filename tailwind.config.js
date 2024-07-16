072b4e6b6d640fa40ff03bbd9bea91e3b423bd69
omarnate1 <omarnate@gmail.com>
2024-07-16 02:18:35 +0100

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
       boxShadow: {
         "custom-16": "10px 10px 0px rgba(0, 0, 0, 100)",
       },
+      colors: {
+        Grey: "#808080",
+        Black: "#121212",
+        Yellow: "#f4d04e",
+      },
     },
   },
   plugins: [],
